92ef42a6206dce770c19a0fd5c86d91fb6903bf5
Wiktor Chomik <wchomik@gmail.com>
2019-02-17 12:33:25 +0100

    Fixed layout flow

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@ function addCampaign(campaign_name, template_name) {
     var source   = document.getElementById(template_name).innerHTML;
     var template = Handlebars.compile(source);
     $.getJSON( "campaigns/" + campaign_name + "/campaign.json", function( data ) {
-            data.characters = []
-        $.when(
-            $.each( data.character_sheets, function( val ) {
-                $.getJSON("campaigns/" + campaign_name + "/characters/" + data.character_sheets[val], function(character_data) {
-                    data.characters.push(character_data);
-                })
-            })
-        ).then(function() {
-            console.log(data);
-            var html    = template(data);
-            $("#content").html(html)
-        })
+        data.characters = []
+
+        $.each( data.character_sheets, function( val ) {
+            $.getJSON("campaigns/" + campaign_name + "/characters/" + data.character_sheets[val], function(character_data) {
+                data.characters.push(character_data);
+                var html    = template(data);
+                $("#content").html(html);
+            });
+        });
     });
 }
 
