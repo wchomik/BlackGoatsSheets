4a4a57bcaa0172069ff7ef6cda1ef3fe8c7d8745
Wiktor Chomik <wchomik@gmail.com>
2019-02-24 00:05:56 +0100

    Migrated to Vue.js, added proper support for different systems

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+function jspath(obj, path) {
+    //console.log(path)
+    var rgx0 = /^([^\[|]*)$/g
+    var rgx1 = /^([^\[|]*)\|(.+)$/g
+    var rgx2 = /^([^\[|]*)\[(.*)\]$/g
+    var match
+    if( match = rgx0.exec(path) ) {
+        //console.log("Match0: " + match[1])
+        return {
+            name: match[1],
+            value: obj[match[1]]
+        }
+    } else if (match = rgx1.exec(path)) {
+        //console.log("Match1: " + match[1] + " " + match[2])
+        return jspath(obj[match[1]], match[2])
+    } else if (match = rgx2.exec(path)) {
+        //console.log("Match2: " + match[1] + " " + match[2])
+        obj = jspath(obj[match[1]], match[2])
+        obj.name = match[1]
+        return obj
+    } else {
+        console.log("Brrrr")
+    }
+}
+
 var translations = {};
 $.ajax({ 
     url: "data/translation_pl.json", 
@@ -16,11 +41,18 @@ $.ajax({
     } 
 });
 
+
+
 var current_lang = "pl";
-function translate(language) {
+
+function translate(phrase) {
+    return translations[current_lang][phrase];
+}
+
+function translate_all(language) {
     current_lang = language
     $("[i18nid]").each(function (){
-        $(this).text(translations[language][$(this).attr("i18nid")])
+        $(this).text(translate($(this).attr("i18nid")))
     })
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
@@ -29,7 +61,7 @@ function translate(language) {
 
 $("[translate]").each(function (){
     $(this).click(function(){
-        translate($(this).attr("translate"))
+        translate_all($(this).attr("translate"))
     })
 })
 
@@ -52,6 +84,7 @@ function calc_derived() {
 }
 
 function calc_character(character_data, template) {
+    //return character_data;
     var template_sheet = sheets_cache[template]
     for(var k in character_data.skills.basic){
         var skill_val = character_data.skills.basic[k]
@@ -112,30 +145,34 @@ function calc_character(character_data, template) {
     return character_data;
 }
 
-var template_cache = {}
 var sheets_cache = {}
-function addCampaign(repo_address, template) {
-    if(!(template in template_cache)) {
-        $.ajax({
-            url: "templates/" + template + ".html",
-            cache: true,
-            async: false,
-            success: function(data) {
-                template_cache[template] = Handlebars.compile(data);
-            }               
-        });
-    }
-    if(!(template in sheets_cache)) {
-        $.ajax({
-            url: "data/" + template + ".json",
-            cache: true,
-            async: false,
-            dataType: 'json',
-            success: function(data) {
-                sheets_cache[template] = data;
-            }               
-        });
-    }
+var campaign_id = 0;
+function addCampaign(repo_address, system) {
+
+    $.ajax({
+        url: "systems/" + system + "/template.html",
+        cache: true,
+        async: true,
+        success: function(data) {
+            //template_cache[system] = Handlebars.compile(data);
+            var html = data.replace(/bgsappid/g, "bgsapp" + campaign_id++)
+            html = html.replace(/bgsrepourl/g, repo_address); 
+            $("#content").html(html);
+        }               
+    });
+
+    return 0;
+    //if(!(template in sheets_cache)) {
+    //    $.ajax({
+    //        url: "data/" + template + ".json",
+    //        cache: true,
+    //        async: false,
+    //        dataType: 'json',
+    //        success: function(data) {
+    //            sheets_cache[template] = data;
+    //        }               
+    //    });
+    //}
     //https://github.com/wchomik/BGS-testcampaing
     //https://github.com/wchomik/BGS-testcampaing/blob/master/campaign.json
     //https://raw.githubusercontent.com/wchomik/BGS-testcampaing/master/campaign.json
@@ -156,7 +193,7 @@ function addCampaign(repo_address, template) {
                     var html    = template_cache[template](campaign);
                     $("#content").html(html);
                     calc_derived();
-                    translate(current_lang);
+                    translate_all(current_lang);
                 });
             });
         }
@@ -167,7 +204,7 @@ function addCampaign(repo_address, template) {
         var html    = template_cache[template](campaign);
         $("#content").html(html);
         calc_derived();
-        translate(current_lang);
+        translate_all(current_lang);
     });
 }
 
